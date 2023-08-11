8e844cd3c26565f45a013cf35bfbd2b6362ccf7f
Kieren James Hussey <khussey45@gmail.com>
2023-08-11 10:34:36 -0400

    stack overflow code - not correct

--- theBook/detab.c
+++ theBook/detab.c
@@ -1,11 +1,33 @@
 // Exercise 1-20
 
-/* Write a program  de-tab that replaces tabs in the input with the proper number of blanks to space to the next tab stop stop. Assume a fixed set of tab stops, say every n columns. Should n be a variable or a symbolic parameter?*/
+/* Write a program  de-tab that replaces tabs in the input with the proper number of blanks to space to the next tab stop. Assume a fixed set of tab stops, say every n columns. Should n be a variable or a symbolic parameter?*/
 
 #include <stdio.h>
 
+#define TAB 8   /* size of a tab */
 
-int main(){
+int main()
+{
+char c;
+int i,column = 0;
 
- 
+while ((c = getchar()) != EOF) {
+    if(c == '\n')
+        column = -1;
+
+    if(column >= TAB)
+        column = 0;
+
+    if (c == '\t') {
+        for (i = column; i < TAB; i++){
+            putchar(' ');
+        }
+        column = -1;
+    }
+    else{
+        putchar(c);
+        }
+    column++;
+}
+return 0;
 }
