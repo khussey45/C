02ada0d891891693c0b295591dbf37bd519033e7
Kieren James Hussey <khussey45@gmail.com>
2023-08-11 10:39:45 -0400

    This seems to be better, but duplicates previous

--- theBook/detab.c
+++ theBook/detab.c
@@ -2,32 +2,30 @@
 
 /* Write a program  de-tab that replaces tabs in the input with the proper number of blanks to space to the next tab stop. Assume a fixed set of tab stops, say every n columns. Should n be a variable or a symbolic parameter?*/
 
+
 #include <stdio.h>
+#include <stdlib.h>
 
-#define TAB 8   /* size of a tab */
+#define TABWIDTH 8
 
-int main()
+int main(void)
 {
-char c;
-int i,column = 0;
+    int i;
+    int c, col, spaces;
 
-while ((c = getchar()) != EOF) {
-    if(c == '\n')
-        column = -1;
-
-    if(column >= TAB)
-        column = 0;
-
-    if (c == '\t') {
-        for (i = column; i < TAB; i++){
-            putchar(' ');
+    col = 0;
+    while((c = getchar()) != EOF) {
+        if (c == '\t') {
+            spaces = TABWIDTH - col % TABWIDTH;
+            for (i = 0; i < spaces; ++i)
+                putchar(' ');
+            col = col + spaces;
+        } else {
+            putchar(c);
+            col = col + 1;
+            if (c == '\n')
+                col = 0;
         }
-        column = -1;
     }
-    else{
-        putchar(c);
-        }
-    column++;
-}
-return 0;
+    return EXIT_SUCCESS;
 }
