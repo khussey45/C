685d5c1a4d995cfd9f29655bdf2afa44cd4a3f23
Kieren James Hussey <khussey45@gmail.com>
2023-08-21 16:16:05 -0400

    converting hello world in c to assembly x86_64

--- theBook/hello.s
+++ theBook/hello.s
@@ -1,14 +1,29 @@
-global    _start
-
-          section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
-
-          section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+	.file	"hello.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Hello, world!\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	puts
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (MinGW-W64 x86_64-ucrt-posix-seh, built by Brecht Sanders) 13.1.0"
+	.def	puts;	.scl	2;	.type	32;	.endef
